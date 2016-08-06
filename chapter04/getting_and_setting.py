9767b4e752888834568619d705e74d7f6e041b26
Nathan Glover <nathan@glovers.id.au>
2016-08-06 09:50:57 -0400

    chapter 4 working fixed

--- chapter04/getting_and_setting.py
+++ chapter04/getting_and_setting.py
@@ -9,8 +9,21 @@ args = vars(ap.parse_args())
 image = cv2.imread(args["image"])
 cv2.imshow("Original", image)
 
+"""
 for x in range(0, image.shape[1]):
     for y in range(0, image.shape[0]):
         (blue, green, red) = image[y, x]
         print("Pixel at (" + str(x) + "," + str(y) + ") - " +
               "Red: {}, Green: {}, Blue: {}".format(red, green, blue))
+"""
+
+# grab 100 x 100 pixel region of the image
+corner = image[0:100, 0:100]
+cv2.imshow("Corner", corner)
+
+# change the colour of this region to green
+image[0:100, 0:100] = (0, 255, 0)
+
+# present the updated image
+cv2.imshow("Updated", image)
+cv2.waitKey(0)
