842c5fbf64413b611b0569ed4a3daffb33c9d2f4
Nathan Glover <nathan@glovers.id.au>
2016-08-06 09:26:26 -0400

    chapter 4 working fixed

--- chapter04/getting_and_setting.py
+++ chapter04/getting_and_setting.py
@@ -10,8 +10,8 @@ args = vars(ap.parse_args())
 image = cv2.imread(args["image"])
 cv2.imshow("Original", image)
 
-for x in range(0, image.shape[0]):
-    for y in range(0, image.shape[1]):
+for x in range(0, image.shape[1]):
+    for y in range(0, image.shape[0]):
         print("x value: " + str(x) +
               " y value: " + str(y) +
               " content: " + pprint.pformat(image[x, y]))
