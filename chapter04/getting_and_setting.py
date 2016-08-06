065366333480176ab7968d42c11dcc30e1e79cac
Nathan Glover <nathan@glovers.id.au>
2016-08-06 09:22:40 -0400

    chapter 4 working

--- chapter04/getting_and_setting.py
+++ chapter04/getting_and_setting.py
@@ -1,6 +1,7 @@
 from __future__ import print_function
 import argparse
 import cv2
+import pprint
 
 ap = argparse.ArgumentParser()
 ap.add_argument("-i", "--image", required=True, help="Path to the image")
@@ -9,3 +10,8 @@ args = vars(ap.parse_args())
 image = cv2.imread(args["image"])
 cv2.imshow("Original", image)
 
+for x in range(0, image.shape[0]):
+    for y in range(0, image.shape[1]):
+        print("x value: " + str(x) +
+              " y value: " + str(y) +
+              " content: " + pprint.pformat(image[x, y]))
