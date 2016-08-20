69c04da1ba3bbf16bc8465a9f246a3cf3731ae41
Nathan Glover <nathan@glovers.id.au>
2016-08-20 04:10:52 -0400

    added line draw

---
+++ chapter05/drawing.py
@@ -0,0 +1,13 @@
+import numpy as np
+import cv2
+
+canvas = np.zeros((300, 300, 3), dtype = "uint8")
+
+green = (0, 255, 0)
+red = (0, 0, 255)
+
+cv2.line(canvas, (0, 0), (300, 300), green)
+cv2.line(canvas, (0, 300), (300, 0), red)
+
+cv2.imshow("Canvas", canvas)
+cv2.waitKey(0)
