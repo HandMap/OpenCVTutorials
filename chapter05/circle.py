860bcec80ef0dcfaae2012c4ed3a063073bdeb9b
Nathan Glover <nathan@glovers.id.au>
2016-08-20 04:41:06 -0400

    added circle

---
+++ chapter05/circle.py
@@ -0,0 +1,14 @@
+import numpy as np
+import cv2
+
+white = (255, 255, 255)
+canvas = np.zeros((300, 300, 3), dtype = "uint8")
+
+# Calculate the (x,y) coordinates of the center of the image
+(centerX, centerY) = (canvas.shape[1] // 2, canvas.shape[0] // 2)
+
+for r in range(0, 175, 25):
+    cv2.circle(canvas, (centerX, centerY), r, white)
+
+cv2.imshow("Canvas", canvas)
+cv2.waitKey(0)
