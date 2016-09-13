f7dfe4243135485e9e980eccae2db61e2e4d1738
Nathan Glover <nathan@glovers.id.au>
2016-09-13 02:30:53 -0400

    added a colour detect test

--- testing/hand_classifier_test/hand-me-the-spoon.py
+++ testing/hand_classifier_test/hand-me-the-spoon.py
@@ -7,7 +7,7 @@ while(cap.isOpened()):
     cv2.rectangle(img,(300,300),(100,100),(0,255,0),0)
     crop_img = img[100:300, 100:300]
     grey = cv2.cvtColor(crop_img, cv2.COLOR_BGR2GRAY)
-    value = (35, 35)
+    value = (5, 5)
     blurred = cv2.GaussianBlur(grey, value, 0)
     _, thresh1 = cv2.threshold(blurred, 127, 255,
                                cv2.THRESH_BINARY_INV+cv2.THRESH_OTSU)
